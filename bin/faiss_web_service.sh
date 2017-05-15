364c70badccdd7c70e64421c3a719d09b9fa7789
Philippe Vinchon <pvinchon@gilt.com>
2017-05-15 23:29:43 +0100

    Start flask app with uwsgi

--- bin/faiss_web_service.sh
+++ bin/faiss_web_service.sh
@@ -2,4 +2,25 @@
 
 ROOT=$(realpath $(dirname ${0})/..)
 
-python ${ROOT}/faiss_web_service/app.py
+development () {
+  python ${ROOT}/faiss_web_service/app.py
+}
+
+production () {
+    mkdir -p /var/log/faiss_web_service
+
+    uwsgi \
+        --http :5000 \
+        --chdir ${ROOT}/faiss_web_service \
+        --module app:app \
+        --master \
+        --processes 4 \
+        --threads 2 \
+        --metric-dir /var/log/faiss_web_service \
+        --logto /var/log/faiss_web_service/app.log
+}
+
+case "${1}" in
+   "production") production ;;
+   *) development ;;
+esac
