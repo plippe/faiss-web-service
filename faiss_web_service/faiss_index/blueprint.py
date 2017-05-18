7bb3b1e673d92beba31af33f417cfa5e6da78828
Philippe Vinchon <pvinchon@gilt.com>
2017-05-18 09:21:09 +0100

    Better error handling

--- faiss_web_service/faiss_index/blueprint.py
+++ faiss_web_service/faiss_index/blueprint.py
@@ -1,4 +1,6 @@
+from jsonschema import validate, ValidationError
 from flask import Blueprint, jsonify, request
+from werkzeug.exceptions import BadRequest
 from faiss_index import FaissIndex
 
 blueprint = Blueprint('faiss_index', __name__)
@@ -11,16 +13,38 @@ def record(setup_state):
         setup_state.app.config['GET_FAISS_IDS_VECTORS'],
         setup_state.app.config.get('UPDATE_FAISS_AFTER_SECONDS'))
 
-@blueprint.route('/faiss')
+@blueprint.route('/faiss/search', methods=['POST'])
 def search():
-    ids = request.args.getlist('ids', type=int)
-    k = request.args.get('k', type=int)
-
     try:
-        results = blueprint.faiss_index.search_by_ids(ids, k)
-        return jsonify(results)
-    except:
-        return jsonify([])
+        json = request.get_json(force=True)
+        validate(json, {
+            'type': 'object',
+            'required': ['k'],
+            'properties': {
+                'k': { 'type': 'integer', 'minimum': 1 },
+                'ids': { 'type': 'array', 'items': { 'type': 'number' }},
+                'vectors': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'array',
+                        'items': { 'type': 'number' }
+                    }
+                }
+            }
+        })
+
+        results_ids = blueprint.faiss_index.search_by_ids(json['ids'], json['k']) if 'ids' in json else []
+        results_vectors = blueprint.faiss_index.search_by_vectors(json['vectors'], json['k']) if 'vectors' in json else []
+
+        return jsonify(results_ids + results_vectors)
+
+    except (BadRequest, ValidationError) as e:
+        print("Bad Request", e)
+        return "Bad Request", 400
+
+    except Exception as e:
+        print("Server error", e)
+        return "Server error", 500
 
 def manage_faiss_index(get_faiss_resources, get_faiss_index, get_faiss_ids_vectors, update_after_seconds):
 
