7ac86da008cc0cf5c41cec939bbe533ded799c5e
123wowow123 <tech@chronopin.com>
2023-01-14 19:47:01 -0800

    seed text

--- src/faiss_index/blueprint.py
+++ src/faiss_index/blueprint.py
@@ -5,13 +5,33 @@ from faiss_index.faiss_index import FaissIndex
 
 blueprint = Blueprint('faiss_index', __name__)
 
+
 @blueprint.record_once
 def record(setup_state):
     blueprint.faiss_index = FaissIndex(
-        setup_state.app.config.get('INDEX_PATH'),
-        setup_state.app.config.get('IDS_VECTORS_PATH'))
+        setup_state.app.config.get('PINS_JSON_PATH')
+        # setup_state.app.config.get('INDEX_PATH'),
+        # setup_state.app.config.get('IDS_VECTORS_PATH')
+    )
 
-@blueprint.route('/faiss/search', methods=['POST'])
+
+@blueprint.route('/faiss/search')
+def search():
+    try:
+        q = request.args.get('q')
+        D, I = blueprint.faiss_index.search_by_sentence(q)
+        return jsonify({'d': D, 'i': I})
+
+    except (BadRequest, ValidationError) as e:
+        print('Bad request', e)
+        return 'Bad request', 400
+
+    except Exception as e:
+        print('Server error', e)
+        return 'Server error', 500
+
+
+@blueprint.route('/faiss/vector/search', methods=['POST'])
 def search():
     try:
         json = request.get_json(force=True)
@@ -19,20 +39,22 @@ def search():
             'type': 'object',
             'required': ['k'],
             'properties': {
-                'k': { 'type': 'integer', 'minimum': 1 },
-                'ids': { 'type': 'array', 'items': { 'type': 'number' }},
+                'k': {'type': 'integer', 'minimum': 1},
+                'ids': {'type': 'array', 'items': {'type': 'number'}},
                 'vectors': {
                     'type': 'array',
                     'items': {
                         'type': 'array',
-                        'items': { 'type': 'number' }
+                        'items': {'type': 'number'}
                     }
                 }
             }
         })
 
-        results_ids = blueprint.faiss_index.search_by_ids(json['ids'], json['k']) if 'ids' in json else []
-        results_vectors = blueprint.faiss_index.search_by_vectors(json['vectors'], json['k']) if 'vectors' in json else []
+        results_ids = blueprint.faiss_index.search_by_ids(
+            json['ids'], json['k']) if 'ids' in json else []
+        results_vectors = blueprint.faiss_index.search_by_vectors(
+            json['vectors'], json['k']) if 'vectors' in json else []
 
         return jsonify(results_ids + results_vectors)
 
--- src/faiss_index/faiss_index.py
+++ src/faiss_index/faiss_index.py
@@ -1,24 +1,63 @@
 import faiss
 import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
 
 class FaissIndex(object):
 
-    def __init__(self, index_path, ids_vectors_path):
-        assert(index_path)
-        assert(ids_vectors_path)
+    # def __init__(self, index_path, ids_vectors_path):
+    #     assert(index_path)
+    #     assert(ids_vectors_path)
 
-        import pickle
-        with open(ids_vectors_path, 'rb') as f:
-            ids_vectors = pickle.load(f, encoding='latin1')
+    #     import pickle
+    #     with open(ids_vectors_path, 'rb') as f:
+    #         ids_vectors = pickle.load(f, encoding='latin1')
 
-        def id_to_vector(id_):
-            try:
-                return ids_vectors[id_]
-            except:
-                pass
+    #     def id_to_vector(id_):
+    #         try:
+    #             return ids_vectors[id_]
+    #         except:
+    #             pass
 
-        self.index = faiss.read_index(index_path)
-        self.id_to_vector = id_to_vector
+    #     self.index = faiss.read_index(index_path)
+    #     self.id_to_vector = id_to_vector
+
+    def __init__(self, json_path):
+        # self.index = faiss.read_index(json_path) ##
+        # self.id_to_vector = id_to_vector
+        self.model = SentenceTransformer('bert-base-nli-mean-tokens')
+        
+        df = pd.read_json(json_path)
+        df['searchColumn'] = df['title'] + " " + df['description']
+        sentences = df['searchColumn'].tolist()
+
+        sentence_embeddings = self.get_embeddings(sentences)
+
+        d = sentence_embeddings.shape[1] # 768
+        nlist = 100
+        bits = 8 # number of bits in each centroid
+        m = 8 # number of centroid IDs in final compressed vectors
+        quantizer = faiss.IndexFlatL2(d)  # this remains the same
+        self.index = faiss.IndexIVFPQ(quantizer, d, nlist, m, bits)
+                                        # 8 specifies that each sub-vector is encoded as 8 bits
+        self.index.train(sentence_embeddings)
+        self.index.add(sentence_embeddings)
+
+        D, I = self.search_by_sentence("war in ukrain") # sanity check
+        print(I)
+        print(D)
+
+    def get_embedding(self, sentence):
+        sentence_embedding = self.model.encode(sentence)
+        return sentence_embedding
+
+    def get_embeddings(self, sentences):
+        sentence_embeddings = self.model.encode(sentences)
+        return sentence_embeddings
+
+
+    def search_by_sentence(self, sentence, k = 4):
+        return self.index.search(sentence, k)
 
     def search_by_ids(self, ids, k):
         vectors = [self.id_to_vector(id_) for id_ in ids]
